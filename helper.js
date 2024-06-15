ef1926a4895f674cd9f52d30af36be5674ba49d2
Abdur Rehman <135581995+Rehman-codes@users.noreply.github.com>
2024-06-15 23:18:10 +0500

    Completed Logic

---
+++ helper.js
@@ -0,0 +1,418 @@
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ZERO's SCENARIOS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+let scenario_1_ZERO = {
+
+    id1: "cell1",
+    boxState1: "zero",
+
+    id2: "cell2",
+    boxState2: "zero",
+
+    id3: "cell3",
+    boxState3: "zero"
+}
+
+let scenario_2_ZERO = {
+
+    id1: "cell4",
+    boxState1: "zero",
+
+    id2: "cell5",
+    boxState2: "zero",
+
+    id3: "cell6",
+    boxState3: "zero"
+}
+
+let scenario_3_ZERO = {
+
+    id1: "cell7",
+    boxState1: "zero",
+
+    id2: "cell8",
+    boxState2: "zero",
+
+    id3: "cell9",
+    boxState3: "zero"
+}
+
+
+let scenario_4_ZERO = {
+
+    id1: "cell1",
+    boxState1: "zero",
+
+    id2: "cell4",
+    boxState2: "zero",
+
+    id3: "cell7",
+    boxState3: "zero"
+}
+
+
+let scenario_5_ZERO = {
+
+    id1: "cell2",
+    boxState1: "zero",
+
+    id2: "cell5",
+    boxState2: "zero",
+
+    id3: "cell8",
+    boxState3: "zero"
+}
+
+
+let scenario_6_ZERO = {
+
+    id1: "cell3",
+    boxState1: "zero",
+
+    id2: "cell6",
+    boxState2: "zero",
+
+    id3: "cell9",
+    boxState3: "zero"
+}
+
+
+let scenario_7_ZERO = {
+
+    id1: "cell1",
+    boxState1: "zero",
+
+    id2: "cell5",
+    boxState2: "zero",
+
+    id3: "cell9",
+    boxState3: "zero"
+}
+
+
+let scenario_8_ZERO = {
+
+    id1: "cell3",
+    boxState1: "zero",
+
+    id2: "cell5",
+    boxState2: "zero",
+
+    id3: "cell7",
+    boxState3: "zero"
+}
+
+export let ZERO_Success_Scenarios = [scenario_1_ZERO,scenario_2_ZERO,scenario_3_ZERO,scenario_4_ZERO,scenario_5_ZERO,scenario_6_ZERO,scenario_7_ZERO,scenario_8_ZERO];
+
+
+let scenario_1_ZERO_Live = {
+
+    id1: "cell1",
+    boxState1: "none",
+
+    id2: "cell2",
+    boxState2: "none",
+
+    id3: "cell3",
+    boxState3: "none"
+}
+
+let scenario_2_ZERO_Live = {
+
+    id1: "cell4",
+    boxState1: "none",
+
+    id2: "cell5",
+    boxState2: "none",
+
+    id3: "cell6",
+    boxState3: "none"
+}
+
+let scenario_3_ZERO_Live = {
+
+    id1: "cell7",
+    boxState1: "none",
+
+    id2: "cell8",
+    boxState2: "none",
+
+    id3: "cell9",
+    boxState3: "none"
+}
+
+
+let scenario_4_ZERO_Live = {
+
+    id1: "cell1",
+    boxState1: "none",
+
+    id2: "cell4",
+    boxState2: "none",
+
+    id3: "cell7",
+    boxState3: "none"
+}
+
+
+let scenario_5_ZERO_Live = {
+
+    id1: "cell2",
+    boxState1: "none",
+
+    id2: "cell5",
+    boxState2: "none",
+
+    id3: "cell8",
+    boxState3: "none"
+}
+
+
+let scenario_6_ZERO_Live = {
+
+    id1: "cell3",
+    boxState1: "none",
+
+    id2: "cell6",
+    boxState2: "none",
+
+    id3: "cell9",
+    boxState3: "none"
+}
+
+
+let scenario_7_ZERO_Live = {
+
+    id1: "cell1",
+    boxState1: "none",
+
+    id2: "cell5",
+    boxState2: "none",
+
+    id3: "cell9",
+    boxState3: "none"
+}
+
+
+let scenario_8_ZERO_Live = {
+
+    id1: "cell3",
+    boxState1: "none",
+
+    id2: "cell5",
+    boxState2: "none",
+
+    id3: "cell7",
+    boxState3: "none"
+}
+
+export let ZERO_Success_Scenarios_Live = [scenario_1_ZERO_Live, scenario_2_ZERO_Live, scenario_3_ZERO_Live, scenario_4_ZERO_Live, scenario_5_ZERO_Live, scenario_6_ZERO_Live, scenario_7_ZERO_Live, scenario_8_ZERO_Live];
+
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< CROSS's SCENARIOS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+let scenario_1_CROSS = {
+
+    id1: "cell1",
+    boxState1: "cross",
+
+    id2: "cell2",
+    boxState2: "cross",
+
+    id3: "cell3",
+    boxState3: "cross"
+}
+
+let scenario_2_CROSS = {
+
+    id1: "cell4",
+    boxState1: "cross",
+
+    id2: "cell5",
+    boxState2: "cross",
+
+    id3: "cell6",
+    boxState3: "cross"
+}
+
+let scenario_3_CROSS = {
+
+    id1: "cell7",
+    boxState1: "cross",
+
+    id2: "cell8",
+    boxState2: "cross",
+
+    id3: "cell9",
+    boxState3: "cross"
+}
+
+
+let scenario_4_CROSS = {
+
+    id1: "cell1",
+    boxState1: "cross",
+
+    id2: "cell4",
+    boxState2: "cross",
+
+    id3: "cell7",
+    boxState3: "cross"
+}
+
+
+let scenario_5_CROSS = {
+
+    id1: "cell2",
+    boxState1: "cross",
+
+    id2: "cell5",
+    boxState2: "cross",
+
+    id3: "cell8",
+    boxState3: "cross"
+}
+
+
+let scenario_6_CROSS = {
+
+    id1: "cell3",
+    boxState1: "cross",
+
+    id2: "cell6",
+    boxState2: "cross",
+
+    id3: "cell9",
+    boxState3: "cross"
+}
+
+
+let scenario_7_CROSS = {
+
+    id1: "cell1",
+    boxState1: "cross",
+
+    id2: "cell5",
+    boxState2: "cross",
+
+    id3: "cell9",
+    boxState3: "cross"
+}
+
+
+let scenario_8_CROSS = {
+
+    id1: "cell3",
+    boxState1: "cross",
+
+    id2: "cell5",
+    boxState2: "cross",
+
+    id3: "cell7",
+    boxState3: "cross"
+}
+
+export let CROSS_Success_Scenarios = [scenario_1_CROSS,scenario_2_CROSS,scenario_3_CROSS,scenario_4_CROSS,scenario_5_CROSS,scenario_6_CROSS,scenario_7_CROSS,scenario_8_CROSS];
+
+
+let scenario_1_CROSS_Live = {
+
+    id1: "cell1",
+    boxState1: "none",
+
+    id2: "cell2",
+    boxState2: "none",
+
+    id3: "cell3",
+    boxState3: "none"
+}
+
+let scenario_2_CROSS_Live = {
+
+    id1: "cell4",
+    boxState1: "none",
+
+    id2: "cell5",
+    boxState2: "none",
+
+    id3: "cell6",
+    boxState3: "none"
+}
+
+let scenario_3_CROSS_Live = {
+
+    id1: "cell7",
+    boxState1: "none",
+
+    id2: "cell8",
+    boxState2: "none",
+
+    id3: "cell9",
+    boxState3: "none"
+}
+
+
+let scenario_4_CROSS_Live = {
+
+    id1: "cell1",
+    boxState1: "none",
+
+    id2: "cell4",
+    boxState2: "none",
+
+    id3: "cell7",
+    boxState3: "none"
+}
+
+
+let scenario_5_CROSS_Live = {
+
+    id1: "cell2",
+    boxState1: "none",
+
+    id2: "cell5",
+    boxState2: "none",
+
+    id3: "cell8",
+    boxState3: "none"
+}
+
+
+let scenario_6_CROSS_Live = {
+
+    id1: "cell3",
+    boxState1: "none",
+
+    id2: "cell6",
+    boxState2: "none",
+
+    id3: "cell9",
+    boxState3: "none"
+}
+
+
+let scenario_7_CROSS_Live = {
+
+    id1: "cell1",
+    boxState1: "none",
+
+    id2: "cell5",
+    boxState2: "none",
+
+    id3: "cell9",
+    boxState3: "none"
+}
+
+
+let scenario_8_CROSS_Live = {
+
+    id1: "cell3",
+    boxState1: "none",
+
+    id2: "cell5",
+    boxState2: "none",
+
+    id3: "cell7",
+    boxState3: "none"
+}
+
+export let CROSS_Success_Scenarios_Live = [scenario_1_CROSS_Live, scenario_2_CROSS_Live, scenario_3_CROSS_Live, scenario_4_CROSS_Live, scenario_5_CROSS_Live, scenario_6_CROSS_Live, scenario_7_CROSS_Live, scenario_8_CROSS_Live];
--- logic.js
+++ logic.js
@@ -1,4 +1,12 @@
-let check = false;
+import { ZERO_Success_Scenarios, ZERO_Success_Scenarios_Live, CROSS_Success_Scenarios, CROSS_Success_Scenarios_Live } from './helper.js';
+
+let turnCross = false;
+let turnCount = 0;
+let winner = "-";
+let victoryZero = false;
+let victoryCross = false;
+let endGame = false;
+let winnerType = document.getElementById("winner-type");
 
 // Storing all the cell elements in an array.
 let cells = document.querySelectorAll(".cell");
@@ -6,24 +14,129 @@ let cells = document.querySelectorAll(".cell");
 // Attaching event handler to all the elements one by one.
 cells.forEach(cell => {
 
-    cell.addEventListener("click", function () {
+  cell.addEventListener("click", function () {
 
-      let cellImage = cell.querySelector("img");
+    if(turnCount<=9 && !endGame)
+    {
+    
+    let cellImage = cell.querySelector("img");
 
-      if(check === false)
+    if (turnCross === false) 
       {
-        cellImage.setAttribute("src", `zero.png`)
-        check = true;
+
+      if (cellImage.getAttribute("src") === "") 
+        {
+        cellImage.setAttribute("src", `zero.png`);
+        cell.setAttribute("data-state", "zero");
+
+        ZERO_Success_Scenarios_Live.forEach(scenario => {
+
+          if (scenario.id1 === cell.getAttribute("id")) 
+            {
+            scenario.boxState1 = "zero";
+            } 
+            else if (scenario.id2 === cell.getAttribute("id")) 
+            {
+            scenario.boxState2 = "zero";
+            } 
+            else if (scenario.id3 === cell.getAttribute("id")) 
+            {
+            scenario.boxState3 = "zero";
+            }
+
+        });
+
+        turnCross = true;
+
+        turnCount++;
+
+        checkWinner();
       }
-      else
-      {
-        cellImage.setAttribute("src", `cross.png`)
-        check = false;
+    } 
+    else 
+    {
+
+      if (cellImage.getAttribute("src") === "") 
+        {
+
+        cellImage.setAttribute("src", `cross.png`);
+        cell.setAttribute("data-state", "cross");
+
+        CROSS_Success_Scenarios_Live.forEach(scenario => {
+
+          if (scenario.id1 === cell.getAttribute("id")) 
+          {
+            scenario.boxState1 = "cross";
+          } 
+          else if (scenario.id2 === cell.getAttribute("id")) 
+          {
+            scenario.boxState2 = "cross";
+          } 
+          else if (scenario.id3 === cell.getAttribute("id")) 
+          {
+            scenario.boxState3 = "cross";
+          }
+
+        });
+
+        turnCross = false;
+
+        turnCount++;
+
+        checkWinner();
       }
+    }
 
-    } );
-})
+  }
+  });
+});
+
+function checkWinner()
+{
+
+  ZERO_Success_Scenarios.forEach(scenario =>{
+
+    ZERO_Success_Scenarios_Live.forEach(livescenario =>{
+
+        if (scenario.boxState1 === livescenario.boxState1 && scenario.boxState2 === livescenario.boxState2 && scenario.boxState3 === livescenario.boxState3) 
+        {
+           victoryZero = true;
+        } 
+
+    })
+
+  })
 
 
+  CROSS_Success_Scenarios.forEach(scenario =>{
+
+    CROSS_Success_Scenarios_Live.forEach(livescenario =>{
+
+        if (scenario.boxState1 === livescenario.boxState1 && scenario.boxState2 === livescenario.boxState2 && scenario.boxState3 === livescenario.boxState3) 
+        {
+           victoryCross = true;
+        } 
+
+    })
+
+  })
 
 
+  if(victoryZero === true)
+  {
+     winner  = "0";
+     endGame = true;
+  }
+  else if(victoryCross === true)
+  {
+    winner  = "x";
+    endGame = true;
+  }
+  else
+  {
+    winner  = "-";
+  }
+
+  winnerType.innerText = winner;
+    
+}
